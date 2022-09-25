980362b0b0ed1c5d309da25aa0cae2b8f9348dfa
Daniele <iximiel@gmail.com>
2022-09-25 19:39:00 +0000

    lost 3 refs

--- topDown/referenceMaker.py
+++ topDown/referenceMaker.py
@@ -1,5 +1,4 @@
 #%%
-from operator import index
 from h5py import File
 from HDF5er import saveXYZfromTrajGroup, MDA2HDF5
 import numpy
@@ -7,11 +6,14 @@ from MDAnalysis import Universe as mdaUniverse
 from SOAPify import (
     saponifyGroup,
     createReferencesFromTrajectory,
+    getReferencesFromDataset,
     mergeReferences,
     SOAPdistanceNormalized,
     saveReferences,
     SOAPReferences,
 )
+from dataclasses import dataclass
+import scipy.cluster.hierarchy as sch
 
 FramesRequest = dict(
     ico923_6=dict(
@@ -29,21 +31,21 @@ FramesRequest = dict(
         b_icoCenter=(0, 0),
     ),
     to807_11_3=dict(
-        s_thVEdge111_111=(0, 723),
+        s_toVEdge111_111=(0, 723),
     ),
     to976_12_4=dict(
-        s_thVer=(0, 974),
-        s_thVEdge001_111=(0, 970),
-        s_thVEdge001_111l=(0, 972),
-        s_thVEdge111_111l=(0, 924),
-        s_th001=(0, 950),
-        s_th001l=(0, 940),
-        s_th111=(0, 676),
-        s_th111l=(0, 920),
-        ss_thVer=(0, 611),
-        ss_th001=(0, 258),
-        ss_th111=(0, 278),
-        b_thFCC_core=(0, 57),
+        s_toVer=(0, 974),
+        s_toVEdge001_111=(0, 970),
+        s_toVEdge001_111l=(0, 972),
+        s_toVEdge111_111l=(0, 924),
+        s_to001=(0, 950),
+        s_to001l=(0, 940),
+        s_to111=(0, 676),
+        s_to111l=(0, 920),
+        ss_toVer=(0, 611),
+        ss_to001=(0, 258),
+        ss_to111=(0, 278),
+        b_toFCC_core=(0, 57),
     ),
     dh1086_7_1_3=dict(
         s_dh5Vert=(0, 10),
@@ -72,51 +74,117 @@ FramesRequest = dict(
 
 
 desiredReferenceOrder = [
-    "s_icoVert",
-    "s_icoEdge",
-    "s_icoEdgel",
-    "s_ico111",
-    "s_ico111l",
-    "ss_ico5FAxis",
-    "ss_icoFCC",
-    "ss_icoHCP",
-    "b_ico5FAxis",
-    "b_icoHCP",
-    "b_icoFCC",
-    "b_icoCenter",
-    "s_thVEdge111_111",
-    "s_thVer",
-    "s_thVEdge001_111",
-    "s_thVEdge001_111l",
-    "s_thVEdge111_111l",
-    "s_th001",
-    "s_th001l",
-    "s_th111",
-    "s_th111l",
-    "ss_thVer",
-    "ss_th001",
-    "ss_th111",
-    "b_thFCC_core",
-    "s_dh5Vert",
-    "s_dhSlimEdgeV",
-    "s_dhEdgelv",
-    "s_dhEdgelc",
-    "s_dhEdge",
-    "s_dhSlimEdge",
-    "s_dhSlimEdgel",
-    "s_dhConcaveEdge",
-    "s_dhconcave",
-    "s_dhconcavel",
-    "s_dh111",
-    "s_dh001",
-    "ss_dhHCP",
-    "ss_dhFCC",
-    "ss_dhFCCnearConcave",
-    "ss_dhHCPnearConcave",
-    "b_dhFCC",
-    "b_dhHCP",
-    "b_dh5folded",
+    "s_icoVert",  # v_ih_5f
+    "s_icoEdge",  # e_ih_(111)
+    "s_icoEdgel",  # e_vih_(111)
+    "s_ico111",  # s_ih_(111)
+    "s_ico111l",  # s_eih_(111)
+    "ss_ico5FAxis",  # ss__5f
+    "ss_icoFCC",  # ss_ih_FCC
+    "ss_icoHCP",  # ss_ih_HCP
+    "b_ico5FAxis",  # b_ih_5f
+    "b_icoHCP",  # b_ih_HCP
+    "b_icoFCC",  # b_ih_FCC
+    "b_icoCenter",  # b_ih_c
+    #
+    "s_toVEdge111_111",  # e_to_(111)
+    "s_toVer",  # v_to
+    "s_toVEdge001_111",  # e_to_(001)
+    "s_toVEdge001_111l",  # e_vto_(001)
+    "s_toVEdge111_111l",  # e_vto_(111)
+    "s_to001",  # s_to_(001)
+    "s_to001l",  # s_eto_(001)
+    "s_to111",  # s_to_(111)
+    "s_to111l",  # s_eto_(111)
+    "ss_toVer",  # ss_to_v
+    "ss_to001",  # ss_to_(001)
+    "ss_to111",  # ss_to_(111)
+    "b_toFCC_core",  # b_to_FCC
+    #
+    "s_dh5Vert",  # v_dh_5f
+    "s_dhSlimEdgeV",  # v__slim
+    "s_dhEdgelv",  # e_vdh_(111)
+    "s_dhEdgelc",  # e_cdh_(111)
+    "s_dhEdge",  # e_dh_(111)
+    "s_dhSlimEdge",  # e__slim
+    "s_dhSlimEdgel",  # s_slim_(111)
+    "s_dhConcaveEdge",  # v_con_(111) ex v__con
+    "s_dhconcave",  # e__con
+    "s_dhconcavel",  # e_v_con
+    "s_dh111",  # s_dh_(111)
+    "s_dh001",  # s_dh_(001)
+    "ss_dhHCP",  # ss_dh_HCP
+    "ss_dhFCC",  # ss_dh_FCC
+    "ss_dhFCCnearConcave",  # ss_con_FCC
+    "ss_dhHCPnearConcave",  # ss_con_HCP
+    "b_dhFCC",  # b_dh_FCC
+    "b_dhHCP",  # b_dh_HCP
+    "b_dh5folded",  # b_dh_5f
 ]
+
+
+@dataclass
+class Names:
+    A: str
+    B: str = ""
+    C: str = " "
+
+    def __call__(self) -> str:
+        toret = "{" + self.A + "}"
+        if self.B != "":
+            toret += "_{\!_{" + self.B + "}}"
+        if self.C != "":
+            toret += "^{" + self.C + "}"
+        return f"${toret}$"
+
+
+renames = dict(
+    s_icoVert=Names("v", "5f", "ih"),
+    s_icoEdge=Names("e", "(111)", "ih"),
+    s_icoEdgel=Names("e", "(111)", "vih"),
+    s_ico111=Names("s", "(111)", "ih"),
+    ss_ico5FAxis=Names("ss", "5f"),
+    ss_icoFCC=Names("ss", "FCC", "ih"),
+    ss_icoHCP=Names("ss", "HCP", "ih"),
+    b_ico5FAxis=Names("b", "5f", "ih"),
+    b_icoHCP=Names("b", "HCP", "ih"),
+    b_icoFCC=Names("b", "FCC", "ih"),
+    b_icoCenter=Names("b", "c", "ih"),
+    #
+    s_toVEdge111_111=Names("e", "(111)", "to"),
+    s_toVer=Names("v", "to"),
+    s_toVEdge001_111=Names("e", "(001)", "to"),
+    s_toVEdge001_111l=Names("e", "(001)", "vth"),
+    s_toVEdge111_111l=Names("e", "(111)", "vth"),
+    s_to001=Names("s", "(001)", "to"),
+    s_to001l=Names("s", "(001)", "e"),
+    s_to111=Names("s", "(111)", "ih"),
+    s_to111l=Names("s", "(111)", "e"),
+    ss_toVer=Names("ss", "v", "to"),
+    ss_to001=Names("ss", "(001)", "ih"),
+    ss_to111=Names("ss", "(111)", "ih"),
+    b_toFCC_core=Names("b", "FCC", "to"),
+    #
+    s_dh5Vert=Names("v", "5f", "dh"),
+    s_dhSlimEdgeV=Names("v", "slim"),
+    s_dhEdgelv=Names("e", "(111)", "vdh"),
+    s_dhEdgelc=Names("e", "(111)", "cdh"),
+    s_dhEdge=Names("e", "(111)", "dh"),
+    s_dhSlimEdge=Names("e", "slim"),
+    s_dhSlimEdgel=Names("s", "(111)", "slim"),
+    s_dhConcaveEdge=Names("v", "con"),
+    s_dhconcave=Names("e", "con"),
+    s_dhconcavel=Names("e", "con", "v"),
+    s_dh111=Names("s", "(111)", "dh"),
+    s_dh001=Names("s", "(001)", "dh"),
+    ss_dhHCP=Names("ss", "HCP", "dh"),
+    ss_dhFCC=Names("ss", "FCC", "dh"),
+    ss_dhFCCnearConcave=Names("ss", "FCC", "con"),
+    ss_dhHCPnearConcave=Names("ss", "HCP", "con"),
+    b_dhFCC=Names("b", "FCC", "dh"),
+    b_dhHCP=Names("b", "HCP", "dh"),
+    b_dh5folded=Names("b", "5f", "dh"),
+)
 
 
 #%%
@@ -124,9 +192,9 @@ for NPID in FramesRequest:
     fname = f"{NPID}_minimized.data"
     u = mdaUniverse(fname, atom_style="id type x y z")
     u.atoms.types = ["Au"] * len(u.atoms)
-    MDA2HDF5(u, "references.hdf5", f"{NPID}", trajChunkSize=1000)
+    MDA2HDF5(u, "referenceFrames.hdf5", f"{NPID}", trajChunkSize=1000)
 
-with File("references.hdf5", "a") as workFile:
+with File("referenceFrames.hdf5", "a") as workFile:
     saponifyGroup(
         trajContainers=workFile["Trajectories"],
         SOAPoutContainers=workFile.require_group("SOAP"),
@@ -140,14 +208,26 @@ with File("references.hdf5", "a") as workFile:
 
 references = dict()
 
-with File("references.hdf5", "r") as workFile:
+with File("referenceFrames.hdf5", "r") as workFile:
     for k in FramesRequest:
         references[k] = createReferencesFromTrajectory(
             workFile[f"SOAP/{k}"], FramesRequest[k], 8, 8
         )
-
 #%%
-allRefs = mergeReferences(*[references[k] for k in references])
+with File("References.hdf5", "w") as refFile:
+    g = refFile.require_group("NPReferences")
+    for k in references:
+        saveReferences(g, k, references[k])
+#%%
+
+myreferences = dict()
+
+with File("References.hdf5", "r") as refFile:
+    g = refFile["NPReferences"]
+    for k in g:
+        myreferences[k] = getReferencesFromDataset(g[k])
+
+allRefs = mergeReferences(*[myreferences[k] for k in myreferences])
 #%% reoredering references
 
 
@@ -164,16 +244,11 @@ reorderedReferences = SOAPReferences(
 
 
 #%%
-with File("References.hdf5", "w") as refFile:
-    g = refFile.require_group("NPReferences")
-    for k in references:
-        saveReferences(g, k, references[k])
-#%%
-import scipy.cluster.hierarchy as sch
 
 
 def referenceDendroMaker(reference):
     ndataset = len(reference)
+    print(ndataset)
     wholeDistances = numpy.zeros((int(ndataset * (ndataset - 1) / 2)))
     cpos = 0
     for i in range(ndataset):
@@ -182,12 +257,18 @@ def referenceDendroMaker(reference):
                 reference.spectra[i], reference.spectra[j]
             )
             cpos += 1
-    sch.dendrogram(sch.linkage(wholeDistances, method="complete"))
+    sch.dendrogram(
+        sch.linkage(wholeDistances, method="complete"),
+        labels=reference.names,
+        orientation="left",
+    )
 
 
 #%%
-icoRefs = mergeReferences(*[references[k] for k in references if "ico" in k])  # OK!
-toRefs = mergeReferences(*[references[k] for k in references if "to" in k])
-dhRefs = mergeReferences(*[references[k] for k in references if "dh" in k])
+icoRefs = mergeReferences(*[myreferences[k] for k in myreferences if "ico" in k])  # OK!
+toRefs = mergeReferences(*[myreferences[k] for k in myreferences if "to" in k])  # OK!
+dhRefs = mergeReferences(
+    *[myreferences[k] for k in myreferences if "dh" in k]
+)  # -3 envs
 #%%
-referenceDendroMaker(toRefs)
+referenceDendroMaker(dhRefs)
