da052b31ec52cc1a551c4b51158e7598f788c74d
Daniele <iximiel@gmail.com>
2022-10-17 14:11:54 +0000

    added  lammps scripts

---
+++ simulations/launcher.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+for file in *.data; do
+    dir=${file%.data}
+    (
+
+	lmp -v name "${dir}" \
+	    -v dataFile "${file}" \
+	    -v Structure "${dir}" \
+	    -v seed_v "${seedV}" \
+	    -v seed_l "${seedL}" \
+	    -log "${dir}.min.log" \
+	    -in minimization.lmp > /dev/null
+	
+	echo "lmp: minimization ${dir} exited with code $?"
+
+	#for reproducing the calculation used in thes paper use
+	# for ico 309
+	# 	seedV=18631
+    # 	seedL=31922
+	# for to 309_9_4
+	# 	seedV=2772
+    # 	seedL=20750
+	# for dh 348_3_2_3
+	# 	seedV=27839
+    # 	seedL=19921
+    seedV=$RANDOM
+    seedL=$RANDOM
+    for Temp in 300 400 500; do
+	simName="${dir}-SV_${seedV}-SL_${seedL}-T_${Temp}"
+	(	    
+	    lmp -v name "${dir}" \
+		-v dataFile "${file}" \
+		-v Structure "${dir}" \
+		-v simName "${simName}" \
+		-v seed_v "${seedV}" \
+		-v seed_l "${seedL}" \
+		-v Temp "${Temp}" \
+		-log "${simName}.eq.log" \
+		-in equilibration.lmp > /dev/null
+	    
+	    echo "lmp: equilibration ${simName} exited with code $?"
+
+	    lmp -v name "${dir}" \
+		-v dataFile "${file}" \
+		-v simName "${simName}" \
+		-v seed_v "${seedV}" \
+		-v seed_l "${seedL}" \
+		-v Temp "${Temp}" \
+		-log "${simName}.log" \
+		-in start.lmp > /dev/null
+	    
+	    echo "lmp: production ${simName} exited with code $?"
+	) &
+    done
+done
